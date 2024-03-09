0c28d01a9c51d1ade55e1a4a7f41aefa6bcef707
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-03-09 10:18:46 +0200

    Add post (login)

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.post('/register',async(req,res)=>{
         // Hash Password 
         const hashedPassword = await bcrypt.hash(password,10)
         users.push({email,password:hashedPassword})
+        console.log(users)
         res.status(201).send("Registered SuccessFully!") // Protocol Registration success
 
     }catch(err){
@@ -40,7 +41,32 @@ app.post('/register',async(req,res)=>{
     }
 })
 
-app.post("")
+
+//     ======================   Login   ======================
+
+app.post("/login",async(req,res)=>{
+    try{
+        const {email,password} = req.body;
+        
+        // Find User 
+        const findUser = users.find((user)=>{
+            return email == user.email
+        })
+        if(!findUser){
+            res.status(400).send("Wrong Email Or Password ! Try Again")
+        }
+        const passwordMatch = await bcrypt.compare(password,findUser.password)
+        if(passwordMatch){
+            res.status(200).send("Logged In Successfully !")
+            console.log(users)
+        }else{
+            res.status(400).send("Wrong Email Or Password ! Try Again")
+        }
+
+    }catch(err){
+        res.status(500).send({message : err.message})
+    }
+})
 
 
 app.listen(port,()=>{
