5da130a4c8f17ac3b3f0e205e566b55454d33fb5
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-03-09 08:25:58 +0200

    Hash Password & Put In LocalData (users)

--- app.js
+++ app.js
@@ -16,11 +16,27 @@ const users = [];
 app.use(express.json())
 
 
-app.post('/register',(req,res)=>{
+app.post('/register',async(req,res)=>{
     try{
 
+        const {email , password} = req.body
+        // Find User 
+        const findUser = users.find((user)=>{
+            return email == user.email
+        })
+
+        if(findUser){
+            res.status(400).send("Wrong Email Or Password ! Try Again")
+        }
+
+        // Hash Password 
+        const hashedPassword = await bcrypt.hash(password,10)
+        users.push({email,password:hashedPassword})
+        res.status(201).send("Registered SuccessFully!") // Protocol Registration success
+
     }catch(err){
-        res.status(500).send(`Internal Server Error ${err}`)
+        // res.status(500).send(`Internal Server Error ${err}`)
+        res.status(500).send({message : err.message})
     }
 })
 
